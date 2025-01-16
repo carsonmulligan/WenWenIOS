8670520c890beede915cbdcc52a6047b81f31cfe
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-01-15 20:46:39 -0500

    Initial Commit

---
+++ WenWen/WenWenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WenWenApp.swift
+//  WenWen
+//
+//  Created by Carson Mulligan on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WenWenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
